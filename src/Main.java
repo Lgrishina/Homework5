59c8e8f8cc133db99df9b728ee0778d401a5a5fe
lgrishina <lgrishina@solvd.com>
2023-03-02 18:08:22 +0300

    homework2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Grishina \nLiudmila \nQA Engineer \nhorse-riding");
+        String entered = "";
+        String password = "пароль";
+        String exit = "выйти";
+        System.out.println("Введите пароль");
+
+       do{
+        Scanner scan = new Scanner(System.in);
+        entered = scan.nextLine();
+        if (!entered.equals(password)&&!entered.equals(exit)){
+        System.out.println("неверный пароль, попробуйте еще раз");}
+       }
+        while(!entered.equals(password)&&!entered.equals(exit));
+
+        if(entered.equals(password)){
+            System.out.println("ура, угадал!");
+        }
     }
 }
