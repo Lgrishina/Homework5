9b350f1fd320f8bb2c9347d6ec0f16da62a439ad
lgrishina <lgrishina@solvd.com>
2023-03-05 15:00:21 +0300

    homework3

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,20 @@
-import java.util.Scanner;
+
+import java.util.Arrays;
 
 public class Main {
+
     public static void main(String[] args) {
-        String entered = "";
-        String password = "пароль";
-        String exit = "выйти";
-        System.out.println("Введите пароль");
+        int[] newArray = {56, 25, 23, 67, 1};
+        System.out.println(Arrays.toString(newArray));
 
-       do{
-        Scanner scan = new Scanner(System.in);
-        entered = scan.nextLine();
-        if (!entered.equals(password)&&!entered.equals(exit)){
-        System.out.println("неверный пароль, попробуйте еще раз");}
-       }
-        while(!entered.equals(password)&&!entered.equals(exit));
-
-        if(entered.equals(password)){
-            System.out.println("ура, угадал!");
+        for (int out = newArray.length - 1; out >= 1; out--){  //Внешний цикл
+            for (int in = 0; in < out; in++){       //Внутренний цикл
+                if(newArray[in] > newArray[in + 1]) // Если порядок сравниваемых переменных нарушен
+                   {int d = newArray[in];      //во временную переменную помещаем первый элемент
+                    newArray[in] = newArray[in+1];       //на место первого ставим второй элемент
+                    newArray[in+1] = d;}
+                }
         }
+        System.out.println(Arrays.toString(newArray) + "- отсортированный массив");
     }
 }
